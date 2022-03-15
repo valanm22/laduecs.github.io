2ad1b8013ec7e7ecfdd29f850ed8c465af803178
Anthony Wang <ta180m@pm.me>
2022-03-15 12:51:46 -0500

    Remove Features from navbar because we aren't a product

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -26,11 +26,6 @@ export const Navigation = (props) => {
         >
           <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href="#features" className="page-scroll">
-                Features
-              </a>
-            </li>
-            <li>
               <a href="#about" className="page-scroll">
                 About
               </a>
