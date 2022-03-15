41b93ab61d6c4a883e4e9b381cbd164ca85be6ae
A T <47871577+spicecat@users.noreply.github.com>
2022-03-15 20:06:49 +0000

    removed features title

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -2,9 +2,6 @@ export const Features = (props) => {
   return (
     <div id='features' className='text-center'>
       <div className='container'>
-        <div className='col-md-10 col-md-offset-1 section-title'>
-          <h2>We Offer</h2>
-        </div>
         <div className='row'>
           {props.data
             ? props.data.map((d, i) => (
