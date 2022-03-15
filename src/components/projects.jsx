47b040787592165759f2ffc619a19770491ebfd8
Anthony Wang <ta180m@pm.me>
2022-03-15 11:32:55 -0500

    Update Projects paragraph

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -6,7 +6,7 @@ export const Projects = (props) => {
       <div className="container">
         <div className="section-title">
           <h2>Projects</h2>
-          <p>Some screenshots of projects</p>
+          <p>Check out our projects!</p>
         </div>
         <div className="row">
           <div className="projects-items">
