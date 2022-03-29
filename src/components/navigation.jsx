3dc05065339c09e60bd7c564b4f2e25f6957db61
Anthony Wang <ta180m@pm.me>
2022-03-28 19:36:37 -0500

    WHY IS THE ICON ON THE RIGHT NOW I want to quit web dev forever OK I give up we're going to have an unconventional layout with that icon

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -15,7 +15,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
             <span className="icon-bar"></span>{" "}
           </button>
-          <img className="navbar-brand" src="img/logo.png" alt="icon" width="50px" height="40px" float="left" />
+          <img src="img/logo.png" alt="icon" width="50px" height="50px" float="left" />
           <a className="navbar-brand page-scroll" href="#page-top">
             LADUE COMPUTER SCIENCE
           </a>{" "}
