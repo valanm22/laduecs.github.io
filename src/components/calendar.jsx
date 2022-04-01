0c6e9d080ae70e8b96ee45a8f55c0a6863d7f860
theVerySharpFlat <aidenramen29@gmail.com>
2022-04-01 02:32:43 +0000

    implement calendar events

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,38 +1,78 @@
 import "./calendar.css"
 import Cal from 'react-calendar';
+import {useState} from 'react'
+import * as csvtojson from 'csvtojson'
 
-
+let csvData = null;
 export const Calendar = (props) => {
+  let value = new Date();
 
+  let calculateEventBoxText = () => {
+    let dateString = `${value.getMonth()+1}/${value.getDate()}/${value.getFullYear()}`;
+    if(csvData === null) {
+      return (<div>loading...</div>);
+    }
+    let entry = csvData.find(element => element[0] === dateString);
+    if(entry !== undefined) {
+      console.log(entry);
+      return (
+        <div>
+          <h2>🎉Meeting Details🎉</h2>
+          <h3>Group True</h3>
+          <p>{entry[1] === "" ? <br/> : entry[1]}</p>
+          <p>{entry[2] === "" ? <br/> : entry[2]}</p>
+          <h3>Group False</h3>
+          <p>{entry[3] === "" ? <br/> : entry[3]}</p>
+          <p>{entry[4] === "" ? <br/> : entry[4]}</p>
+        </div>  
+      )
+    }
+    return (<div><h2>No Meeting</h2></div>)
+  }
+
+  let [eventBoxText, setEventBoxText] = useState(calculateEventBoxText());
+  let calendarChangeCallback = (val, event) => {
+    console.log(val);
+    value = val;
+    setEventBoxText(calculateEventBoxText());
+  };
+
+  if(csvData === null) {
     fetch( "schedule.csv" )
     .then(async response => {
       const reader = response.body.getReader()
       const result = await reader.read() // raw array
       const decoder = new TextDecoder('utf-8')
       const csv = decoder.decode(result.value)
-      console.log(csv);
+      csvtojson({
+        output: "csv"
+      })
+      .fromString(csv)
+      .then(function(result){
+        csvData = result;
+        setEventBoxText(calculateEventBoxText());
+      })
+      .catch(err => {
+        console.log(err);
+      })
     }).catch(err => {
       console.error(err);
     });
-
-  let value = new Date();
-  let calendarChangeCallback = (val, event) => {
-    console.log(val);
-    value = val;
-  };
+  }
+  
 
   return (
-      <div class="calendar">
-        <div className="text-center section-title">
-          <h2>Calendar</h2>
-        </div>
+      <div class="calendar" style={{marginTop: "5em"}}>
         <div class="container text-center calendar-container" style={{paddingBottom: "10px"}}>
+          <div className="text-center section-title" style={{marginBottom: "5em"}}>
+            <h2>Calendar</h2>
+          </div>
           <div class="row" style={{height: "auto"}}>
             <div align="center" class="col-md-6" style={{marginBottom: "100px"}}>
               <Cal onChange={calendarChangeCallback} value={value} />
             </div>
             <div class="col-md-6" style={{height: "auto"}}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis error reiciendis esse perspiciatis, iusto laboriosam rerum nisi beatae tempora repellendus ea officia pariatur vitae fugiat aliquid dolorem saepe sit autem deserunt dolorum eos. Minima ipsa excepturi, at ea, voluptate nemo ipsam, omnis aperiam eius unde facilis consequuntur doloremque modi quam.
+              {eventBoxText}             
             </div>
           </div>
         </div>
